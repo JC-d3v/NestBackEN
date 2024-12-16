813ce75a43e2ac40546cf2bf333f18faa3c4e9a0
Jorge Calderon <calderon.rjorge@gmail.com>
2024-12-16 11:47:33 -0400

    S2 Tipos en Arreglos

---
+++ 01-TypeScript-Intro/src/bases/02-objects.ts
@@ -0,0 +1,23 @@
+export const pokemonIds = [1, 20, 30, 34, 66];
+
+// pokemonIds.push('qwerty');
+
+interface Pokemon {
+  id: number;
+  name: string;
+  age: number;
+}
+
+export const bulbasor: Pokemon = {
+  id: 1,
+  name: 'Bulbasor',
+  age: 2
+}
+
+export const charmander: Pokemon = {
+  id: 3,
+  name: 'Charmander',
+  age: 3
+}
+
+console.log(bulbasor);
--- 01-TypeScript-Intro/src/main.ts
+++ 01-TypeScript-Intro/src/main.ts
@@ -1,27 +1,15 @@
+import { bulbasor } from './bases/02-objects'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.ts'
 
-import { name, age, isValid } from './bases/01-types'
+// import { name, age, isValid } from './bases/01-types'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-  <h1>hello ${name} ${age} ${isValid}</h1>  
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+  <h1>hello VITE ${bulbasor.name}</h1>  
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
