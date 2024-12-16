c44f8e5a5e9888b9946d113e4eaf43e8fceee4f1
Jorge Calderon <calderon.rjorge@gmail.com>
2024-12-16 10:53:10 -0400

    S2 Objetos e interfaces

--- 01-TypeScript-Intro/src/bases/01-types.ts
+++ 01-TypeScript-Intro/src/bases/01-types.ts
@@ -1,5 +1,22 @@
-export const name: string = 'Jorge';
+export let name: string = 'Jorge';
 
 export const age: number = 333
 
 export const isValid: boolean = true;
+
+
+name = 'TEST';
+
+
+export const templateString = `Esto es un string
+multilinea
+que puede tener 
+" dobles
+' Simples
+y permite inyeccion de valores ${name}
+expresiones ${1 + 1}
+numeros ${age}
+booleanos ${isValid}
+`
+
+console.log(templateString);
