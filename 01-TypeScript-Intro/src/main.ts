1703a4438f70c0563f9a2dd7bdf47b315ecd4c1f
Jorge Calderon <calderon.rjorge@gmail.com>
2024-12-18 12:54:11 -0400

    S2 Genericos + sustitucion de liskov

--- 01-TypeScript-Intro/src/main.ts
+++ 01-TypeScript-Intro/src/main.ts
@@ -1,5 +1,7 @@
 // import { pokemons } from './bases/02-objects'
-import { charmander } from './bases/03-clases'
+// import { charmander } from './bases/03-clases'
+import { charmander } from './bases/04-injection'
+
 import './style.css'
 // import typescriptLogo from './typescript.svg'
 // import viteLogo from '/vite.svg'
