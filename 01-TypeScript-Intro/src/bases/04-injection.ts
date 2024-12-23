d9581474abb62df68743567303bd80d3d5b1b7b1
Jorge Calderon <calderon.rjorge@gmail.com>
2024-12-23 15:16:12 -0400

    S2 Resolver el principio de sustitucion

--- 01-TypeScript-Intro/src/bases/04-injection.ts
+++ 01-TypeScript-Intro/src/bases/04-injection.ts
@@ -1,5 +1,6 @@
-import { PokeApiAdapter } from '../api/pokeApi.adapter';
+import { PokeApiAdapter, PokeApiFetchAdapter } from '../api/pokeApi.adapter';
 import { Move, PokeapiResponseInterface } from '../interfaces/pokeapi-response.interface';
+import { name } from './01-types';
 
 export class Pokemon {
 
@@ -25,7 +26,7 @@ export class Pokemon {
 
   async getMoves(): Promise<Move[]> {
     // const { data } = await axios .get<PokeapiResponseInterface>('https://pokeapi.co/api/v2/pokemon/4');
-    const data = await this.http.get('https://pokeapi.co/api/v2/pokemon/4');
+    const data = await this.http.get<PokeapiResponseInterface>('https://pokeapi.co/api/v2/pokemon/4');
     console.log(data.moves);
 
     return data.moves;
@@ -34,7 +35,8 @@ export class Pokemon {
 }
 
 const pokeApi = new PokeApiAdapter();
+const pokeApiFetch = new PokeApiFetchAdapter();
 
-export const charmander = new Pokemon(4, 'Charmander', pokeApi);
+export const charmander = new Pokemon(4, 'Charmander', pokeApiFetch);
 
 charmander.getMoves();
