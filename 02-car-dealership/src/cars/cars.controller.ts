b91bee67f0476fcf22cf5dd0c00b5f9bb3184820
Jorge Calderon <calderon.rjorge@gmail.com>
2025-04-06 23:53:09 -0400

    S3 Injeccion de dependencias

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -3,7 +3,6 @@ import { Controller, Get, Param } from '@nestjs/common';
 @Controller('cars')
 export class CarsController {
 
-  private cars = ['Toyota', 'Honda', 'Jeep'];
 
   @Get()
   getAllCars() {
