d0e804636555dcede4ac5dab93e81cce62e63295
Jorge Calderon <calderon.rjorge@gmail.com>
2025-04-14 14:18:09 -0400

    S4 - Pipe ParseUUIDPipe

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -15,7 +15,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id', ParseIntPipe) id: number) {
+  getCarById(@Param('id') id: string) {
     console.log({ id });
     return this.carsService.findOneById(id);
   }
@@ -27,13 +27,13 @@ export class CarsController {
 
   @Patch(':id')
   updateCar(
-    @Param('id', ParseIntPipe) id: number,
+    @Param('id') id: number,
     @Body() body: any) {
     return body;
   }
 
   @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: number) {
+  deleteCar(@Param('id') id: number) {
     return {
       method: 'delete',
       id
