755af624c3b68452f5c9137782e54fc584caf722
Jorge Calderon <calderon.rjorge@gmail.com>
2025-05-12 13:16:29 -0400

    S7 - Recibir y validar la data

--- 03-pokedex/src/pokemon/entities/pokemon.entity.ts
+++ 03-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,25 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+
+  // id: string;  asignado por mongo
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  nro: number;
+
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
