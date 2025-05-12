896e47482d16afd70951dbf997c466a16d259a90
Jorge Calderon <calderon.rjorge@gmail.com>
2025-05-12 13:06:04 -0400

    S7 - Crear esquemas y modelos

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -2,6 +2,7 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -9,6 +10,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    MongooseModule.forRoot('mongodb+srv://rjcv:Password1.@nestbe.8lkvi9e.mongodb.net/nest-pokemon'),
 
     PokemonModule
   ],
