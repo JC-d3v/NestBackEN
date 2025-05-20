83af72f20522b65fee232d0a81b7afa3d43ce134
Jorge Calderon <calderon.rjorge@gmail.com>
2025-05-20 11:32:13 -0700

    S7 - Validar y eliminar en una sola consulta

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -13,7 +14,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
     MongooseModule.forRoot('mongodb+srv://rjcv:Password1.@nestbe.8lkvi9e.mongodb.net/nest-pokemon'),
 
-    PokemonModule
+    PokemonModule,
+
+    CommonModule
   ],
 })
 export class AppModule { }
--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -85,8 +85,14 @@ export class PokemonService {
 
   async remove(id: string) {
 
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    // const pokemon = await this.findOne(id);
+    // await pokemon.deleteOne();
+
+    // return { id }
+
+    const result = this.pokemonModel.findByIdAndDelete(id);
+
+    return result;
 
   }
 
