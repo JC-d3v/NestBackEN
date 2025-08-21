b7f9505366a5ee7be263c56e4c8d21bb748daaeb
Jorge Calderon <calderon.rjorge@gmail.com>
2025-08-21 09:26:55 -0700

    S11 Eliminacion en Cascada

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -108,13 +108,36 @@ export class ProductsService {
 
     // Create query Runner
     const queryRunner = this.dataSource.createQueryRunner();
-
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
     try {
-      await this.productRepository.save(product);
-      return product
+
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+
+        product.images = images.map(
+          image => this.productImageRepository.create({ url: image })
+        )
+      } else {
+        // 
+        // product.images
+      }
+
+      await queryRunner.manager.save(product);
+
+
+      // await this.productRepository.save(product);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+      // return product
+      return this.findOnePlain(id);
 
     } catch (error) {
+
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+
       this.handleDBExceptions(error)
     }
 
