c32d69b8c89d57b3404d3257d751412051baaae3
Jorge Calderon <calderon.rjorge@gmail.com>
2025-09-04 08:44:52 -0700

    S13 Nest Authenticatoni Passport

--- 04-teslo-shop/src/auth/auth.service.ts
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -1,11 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import * as bcrypt from 'bcrypt';
 
-import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { LoginUserDto, CreateUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -52,6 +52,34 @@ export class AuthService {
     this.logger.error(error)
     // console.log({ error });
     throw new InternalServerErrorException('Unexcpeced error check server logs!!!');
+
+
+  }
+
+
+
+  async login(loginUserDto: LoginUserDto) {
+
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: {
+        email: true,
+        password: true
+      }
+    })
+
+    if (!user)
+      throw new UnauthorizedException('Credencials are not valid (email) ')
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credencials are not valid (password) ')
+
+    return user;
+
+    // TODO: retornar el JWT
+
 
 
   }
